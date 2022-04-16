899c87ad3c9f6d09b5e75c38ff614686721d3752
macasa <macasa@zend-macasa.fjeclot.net>
2022-04-16 15:59:59 +0200

    Menu Principal, Buscar Usuari i primer pauqet zpk

---
+++ menu.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title>
+			PÀGINA WEB DEL MENÚ PRINCIPAL DE L'APLICACIÓ D'ACCÉS A BASES DE DADES LDAP
+		</title>
+	</head>
+	<body>
+		<h2> MENÚ PRINCIPAL DE L'APLICACIÓ D'ACCÉS A BASES DE DADES LDAP</h2>
+		<h3> <b>Opcions</b> </h3>
+		<a href="http://zend-macasa.fjeclot.net/projecteM08/buscarUsuari.php">Buscar Usuari</a>
+		<a href="http://zend-macasa.fjeclot.net/projecteM08/afegirUsuari.php">Afegir Usuari</a>
+		<a href="http://zend-macasa.fjeclot.net/projecteM08/esborrarUsuari.php">Modificar Usuari</a>
+		<a href="http://zend-macasa.fjeclot.net/projecteM08/esborrarUsuari.php">Esborrar Usuari</a>
+
+		<a href="http://zend-macasa.fjeclot.net/projecteM08/index.php">Torna a la pàgina inicial</a>
+	</body>
+</html>
